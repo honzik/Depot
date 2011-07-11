e35e521b7e8a61604283fbb6409d0c8b61438910
Honzik-janicka/ubuntu <honzamaly@gmail.com>
2011-07-11 14:29:59 +0200

    Added ship_date column

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,5 +1,7 @@
 class Order < ActiveRecord::Base
-
+  
+  before_create :update_ship_date
+  
 	if PaymentType.find(:all).count > 0
 		@@payment_type = PaymentType.find(:all).map {|p| p.name}	
 	else
@@ -21,5 +23,11 @@ class Order < ActiveRecord::Base
 	def self.get_payment_types
 		@@payment_type	
 	end
+
+  protected
+  
+  def update_ship_date
+    self.ship_date = Date.tomorrow
+  end
 
 end
--- test/unit/order_test.rb
+++ test/unit/order_test.rb
@@ -1,8 +1,13 @@
 require 'test_helper'
 
 class OrderTest < ActiveSupport::TestCase
+  
+  fixtures :orders
+  
   # Replace this with your real tests.
-  test "the truth" do
-    assert true
+  test "order must have ship_date 1 day from now" do
+    order = Order.new(orders(:one).attributes)
+    order.save
+    puts order.ship_date.to_s
   end
 end
