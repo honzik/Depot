1fc48d8307b2b66200116df3a699d8ef57b7dc61
Honzik-janicka/ubuntu <honzamaly@gmail.com>
2011-07-11 16:19:00 +0200

    Added integration tests for invalid order reporting and ship_date change notifying

--- test/integration/user_stories_test.rb
+++ test/integration/user_stories_test.rb
@@ -1,7 +1,7 @@
 require 'test_helper'
 
 class UserStoriesTest < ActionDispatch::IntegrationTest
-  fixtures :products
+  fixtures :products, :orders
   
   # buying a product in an empty environment
   test "buying a product" do
@@ -56,4 +56,48 @@ class UserStoriesTest < ActionDispatch::IntegrationTest
     assert_equal 'Pragmatic Store Order Confirmation', mail.subject       
     
   end
+  
+  # sending error on failure of orders
+  test "report wrong order ID" do    
+    misplaced_id = 666
+    
+    # get index
+    get_via_redirect "/orders/" + misplaced_id.to_s
+    assert_response :success
+    assert_template "index"
+    
+    mail = ActionMailer::Base.deliveries.last
+    assert_equal ["honzik@localhost"], mail.to
+    assert_match /Invalid order/, mail.subject 
+    assert_match /666/, mail.body.encoded
+   
+  end
+  
+  # buying a product in an empty environment
+  test "update a ship date" do
+    order = orders(:one)
+    
+    # get index
+    get edit_order_path(order)
+    
+    assert_response :success
+    assert_template "edit"
+   
+    # order data 
+    edited_order = { 
+                  :name     => order.name, 
+                  :address  => order.address,
+                  :email    => order.email,
+                  :pay_type => order.pay_type, 
+                  :ship_date=> order.ship_date + 1
+                  }
+
+    put_via_redirect order_path(order), :order => edited_order    
+    mail = ActionMailer::Base.deliveries.last
+    assert_equal [edited_order[:email]], mail.to
+    assert_match /ship date changed/, mail.subject  
+    assert_match /#{Regexp.escape(edited_order[:ship_date].strftime("%A"))}/, mail.body.encoded
+   
+  end
+  
 end
--- test/unit/order_test.rb
+++ test/unit/order_test.rb
@@ -5,7 +5,7 @@ class OrderTest < ActiveSupport::TestCase
   fixtures :orders
   
   # Replace this with your real tests.
-  test "order must have ship_date 1 day from now" do
+  test "a created order must have ship_date 1 day from now" do
     order = Order.new(orders(:one).attributes)
     order.save
     assert_equal order.ship_date, Date.tomorrow
