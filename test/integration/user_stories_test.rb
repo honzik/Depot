079bfc9e55f12541f2c2400ea6c4b41f9eaf4fcb
Honzik-janicka/ubuntu <honzamaly@gmail.com>
2011-07-14 18:12:59 +0200

    Fixed integration tests, added login/logout integration case

--- test/integration/user_stories_test.rb
+++ test/integration/user_stories_test.rb
@@ -3,6 +3,17 @@ require 'test_helper'
 class UserStoriesTest < ActionDispatch::IntegrationTest
   fixtures :products, :orders, :users
   
+  # log user :one in
+  def log_user_in
+    user = users(:one);        
+    get "/login"  
+    login_data = { 
+                  :name     => user.name, 
+                  :password  => 'secret'
+                 }   
+    post_via_redirect "/login", login_data
+  end
+  
   # buying a product in an empty environment
   test "buying a product" do
     LineItem.delete_all
@@ -57,45 +68,60 @@ class UserStoriesTest < ActionDispatch::IntegrationTest
     
   end
   
-#  # sending error on failure of orders
-#  test "report wrong order ID" do        
-#    misplaced_id = 666
-#    
-#    # get index
-#    get_via_redirect "/orders/" + misplaced_id.to_s
-#    assert_response :success
-#    
-#    mail = ActionMailer::Base.deliveries.last
-#    assert_equal ["honzik@localhost"], mail.to
-#    assert_match /Invalid order/, mail.subject 
-#    assert_match /666/, mail.body.encoded
-#  
-#  end
-#  
-#  # buying a product in an empty environment
-#  test "update a ship date" do
-#    order = orders(:one)
-#        
-#    # get index
-#    get edit_order_path(order)
-#    
-#    assert_response :success
-#   
-#    # order data 
-#    edited_order = { 
-#                  :name     => order.name, 
-#                  :address  => order.address,
-#                  :email    => order.email,
-#                  :pay_type => order.pay_type, 
-#                  :ship_date=> order.ship_date + 1
-#                  }
+  # testing login, logout success
+  test "login, logout" do        
+    log_user_in         
+    assert_response :success            
+    assert_equal '/admin', path
+    
+    delete_via_redirect("/logout")
+    assert_response :success            
+    assert_equal '/', path         
+  end
+  
+  # sending error on failure of orders
+  test "report wrong order ID" do        
+    log_user_in
+    misplaced_id = 666
+    
+    # get index
+    get_via_redirect "/orders/" + misplaced_id.to_s
+    assert_response :success
+    assert_template "index"
+    
+    mail = ActionMailer::Base.deliveries.last
+    assert_equal ["honzik@localhost"], mail.to
+    assert_match /Invalid order/, mail.subject 
+    assert_match /666/, mail.body.encoded
+  
+  end
+  
+  # buying a product in an empty environment
+  test "update a ship date" do
+    log_user_in
+    order = orders(:one)
+        
+    # get index
+    get edit_order_path(order)
+    
+    assert_response :success
+    assert_template "edit"
+    
+    # order data 
+    edited_order = { 
+                  :name     => order.name, 
+                  :address  => order.address,
+                  :email    => order.email,
+                  :pay_type => order.pay_type, 
+                  :ship_date=> order.ship_date + 1
+                  }
 
-#    put_via_redirect order_path(order), :order => edited_order    
-#    mail = ActionMailer::Base.deliveries.last
-#    assert_equal [edited_order[:email]], mail.to
-#    assert_match /ship date changed/, mail.subject  
-#    assert_match /#{Regexp.escape(edited_order[:ship_date].strftime("%A"))}/, mail.body.encoded
-#   
-#  end
+    put_via_redirect order_path(order), :order => edited_order    
+    mail = ActionMailer::Base.deliveries.last
+    assert_equal [edited_order[:email]], mail.to
+    assert_match /ship date changed/, mail.subject  
+    assert_match /#{Regexp.escape(edited_order[:ship_date].strftime("%A"))}/, mail.body.encoded
+   
+  end
   
 end
