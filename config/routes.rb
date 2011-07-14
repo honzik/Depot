d05e75e34ce0bce27880672c3c97490ac1e15e21
Honzik-janicka/ubuntu <honzamaly@gmail.com>
2011-07-14 11:23:41 +0200

    Added session handling for login/logout of users

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Depot::Application.routes.draw do
+  get 'admin' => 'admin#index'
+  
+  controller :sessions do
+    get 'login' => :new
+    post 'login' => :create
+    delete 'login' => :destroy
+  end
+
   resources :users
 
   resources :orders
